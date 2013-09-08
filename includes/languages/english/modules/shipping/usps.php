081befd0d8cb6e962ccac6519de4621089edc045
Evan-R <evan.gowfb@gmail.com>
2013-09-08 03:01:13 -0600

    update languages file

--- includes/languages/english/modules/shipping/usps.php
+++ includes/languages/english/modules/shipping/usps.php
@@ -5,14 +5,14 @@ $Id: usps.php 1739 2011-09-01 00:52:16Z hpdl $
 osCommerce, Open Source E-Commerce Solutions
 http://www.oscommerce.com
 
-Copyright - 2002 osCommerce
+Copyright © 2002 osCommerce
 
 Released under the GNU General Public License
 */
 
 define('MODULE_SHIPPING_USPS_TEXT_TITLE', 'United States Postal Service&nbsp;');
-define('MODULE_SHIPPING_USPS_TEXT_DESCRIPTION', 'United States Postal Service<br><br>You will need to have registered an account with USPS at https://secure.shippingapis.com/registration/ to use this module<br><br>USPS expects you to use pounds as weight measure for your products.');
-define('MODULE_SHIPPING_USPS_TEXT_ERROR', 'An error occured with the USPS shipping calculations.<br>If you would like to use USPS as your shipping method, please contact the store owner.');
+define('MODULE_SHIPPING_USPS_TEXT_DESCRIPTION', 'United States Postal Service<br /><br />You will need to have registered an account with USPS at https://secure.shippingapis.com/registration/ to use this module<br /><br />USPS expects you to use pounds as weight measure for your products.');
+define('MODULE_SHIPPING_USPS_TEXT_ERROR', 'An error occured with the USPS shipping calculations.<br />If you would like to use USPS as your shipping method, please contact the store owner.');
 define('MODULE_SHIPPING_USPS_TEXT_DAY', 'day');
 define('MODULE_SHIPPING_USPS_TEXT_DAYS', 'days');
 define('MODULE_SHIPPING_USPS_TEXT_WEEKS', 'weeks');
@@ -21,5 +21,6 @@ define('MODULE_SHIPPING_USPS_TEXT_VARIES', 'varies by country');
 define('MODULE_SHIPPING_USPS_TEXT_ESTIMATED', 'Approximate delivery time from mailing date ');
 define('MODULE_SHIPPING_USPS_TEXT_INSURED', 'Insured for ');
 define('MODULE_SHIPPING_USPS_TEXT_WEIGHT_DISPLAY', ' (%d pkg, %01.2f lbs total)');
-define('MODULE_SHIPPING_USPS_TEXT_CONNECTION_ERROR', 'Failed to connect to the USPS postage rates server.');
+define('MODULE_SHIPPING_USPS_TEXT_CONNECTION_ERROR', 'Failed to connect to the USPS Web Tools&trade; postage rates server.');
+define('MODULE_SHIPPING_USPS_ERROR_NO_SERVICES', 'No allowed USPS services could be used to ship this package.<br />If you would like to use USPS as your shipping method, please contact the store owner.');
 
